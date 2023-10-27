53b21b4c6bdff7abde2703e371711f0ea760a915
Loi Nguyen <113363230+LoiNguyenCS@users.noreply.github.com>
2023-10-27 19:16:04 -0400

    add clarifying comments

--- download_caption.py
+++ download_caption.py
@@ -3,30 +3,22 @@ from pytube import YouTube
 
 import os
 
-# Define the URL of the YouTube playlist
-playlist_url = 'https://www.youtube.com/watch?v=kRHhZ8l9RMQ&list=PLp-wXwmbv3z9Xj172jZPPPoYeTS0eanty&pp=iAQB'
 
-# Create a Pytube Playlist object
+playlist_url = 'https://www.youtube.com/watch?v=kRHhZ8l9RMQ&list=PLp-wXwmbv3z9Xj172jZPPPoYeTS0eanty&pp=iAQB'
 playlist = Playlist(playlist_url)
 
-# Set the directory where you want to save the caption files
-output_directory = 'captions/'
 
-# Create the output directory if it doesn't exist
+output_directory = 'captions/'
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
 count = 0
-# Iterate through the videos in the playlist and download captions
 for video in playlist.video_urls:
     if count > 50:
       break;
     yt = YouTube(video)
     try:
       yt.bypass_age_gate()
-      print('Captions Available: ', yt.captions)
-      print()
-
       en_caption = yt.captions.get("a.en")
       video_title = yt.title
       caption_filename = os.path.join(output_directory, f"{video_title}.xml")
@@ -35,6 +27,7 @@ for video in playlist.video_urls:
       count = count + 1
 
     except:
-      print("You're too young")
+      # The current video has an age restriction. The program will skip it
+      print("Skip one video due to age restriction!")
 
 print("Caption download completed.")
