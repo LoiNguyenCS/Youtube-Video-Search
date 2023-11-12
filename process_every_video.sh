92c2e2f500f8215273ac3d961403ebc06bcc2f84
Loi Nguyen <113363230+LoiNguyenCS@users.noreply.github.com>
2023-11-12 17:21:57 -0500

    Update process_every_video.sh

--- process_every_video.sh
+++ process_every_video.sh
@@ -4,6 +4,6 @@ input_folder="$1"
 for video_path in "$input_folder"/*/*.mp4; do
     echo "$video_path"
     if [ -f "$video_path" ]; then
-        python3 video-indexing.py "$video_path" 
+        python3 video-encoding.py "$video_path" 
     fi
 done
